9417af57df5f4835566dcfeeabc5e3c531b5a79d
Denis <namorado@TFGcos-MacBook-Pro.local>
2021-07-21 13:52:21 -0300

    pre-deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
-let publicPath = process.env.NODE_ENV === 'production' ? 'light-blue-vue-admin/' : '/';
+// let publicPath = process.env.NODE_ENV === 'production' ? 'light-blue-vue-admin/' : '/';
+
+// module.exports = {
+//   publicPath,
+//   productionSourceMap: false,
+// };
+
 
 module.exports = {
-  publicPath,
-  productionSourceMap: false,
-};
+  publicPath: '/papaiz/'}
